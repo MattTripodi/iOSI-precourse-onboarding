79c68fb9c706ff34f7588e0d0295ddf3b2b5b748
Matt Tripodi <mtripodi1027@gmail.com>
2016-10-10 20:07:05 -0400

    GAPreworkProject

---
+++ 01-task/MattTripodi-precourse-app/MattTripodi-precourse-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MattTripodi-precourse-app
+//
+//  Created by Matt Tripodi on 10/10/16.
+//  Copyright © 2016 Matt Tripodi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
